04d96a7ef2104a6181dae9b9840caf6d65451594
Louis-Deglin <louis.deglin@hotmail.com>
2026-05-15 09:45:49 +0200

    Create Script_clean_8_Afrikaner.sh

---
+++ GLnexus/Script_clean_8_Afrikaner.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH -A hpc2n2025-261
+#SBATCH -J Clean_Afri
+#SBATCH -t 00:30:00
+#SBATCH -n 1
+#SBATCH -c 4
+#SBATCH --mem=16G
+#SBATCH -o logs/clean_afrikaner_%j.out
+#SBATCH -e logs/clean_afrikaner_%j.err
+#SBATCH --mail-user=louis.deglin@slu.se
+#SBATCH --mail-type=ALL
+
+set -e
+set -o pipefail
+
+module load GCC/13.2.0 BCFtools/1.19
+
+export BREEDMAPS="/pfs/proj/nobackup/fs/projnb10/hpc2nstor2025-071"
+
+BASE_DIR="$BREEDMAPS/louis/results/DeepVariant/Afrikaner_ARS2.0"
+
+for ID in 025 026 027 028 029 030 031 032; do
+
+    DIR="${BASE_DIR}/pr_219_${ID}"
+    ORIGINAL_VCF="${DIR}/pr_219_${ID}.deepvariant.g.vcf.gz"
+    CLEANED_VCF="${DIR}/pr_219_${ID}.clean.g.vcf.gz"
+
+    bcftools view -h $ORIGINAL_VCF > ${DIR}/header.txt
+
+    grep -v "ID=NKLS" ${DIR}/header.txt > ${DIR}/clean_header.txt
+
+    bcftools reheader -h ${DIR}/clean_header.txt -o $CLEANED_VCF $ORIGINAL_VCF
+
+    bcftools index -t $CLEANED_VCF
+
+    rm -f ${DIR}/header.txt ${DIR}/clean_header.txt
+
+done
+
+echo "=== Nettoyage terminé pour les Afrikaner === $(date)"
